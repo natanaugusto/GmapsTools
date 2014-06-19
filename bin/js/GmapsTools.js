8aca0eb503eabf778e41d95ad95916a73a2bcc37
Natan Augusto <natanaugusto@gmail.com>
2014-06-19 02:46:35 -0300

    WORKING - Desenvolvendo a biblioteca

--- bin/js/GmapsTools.js
+++ bin/js/GmapsTools.js
@@ -127,247 +127,89 @@ var GmapsTools = {
             content: 'New InfoWindow',
             closed: false
         }
-    },
-    /**
-     * Cria um novo objeto Gmap relacionado a uma div
-     * @param div HTML Element
-     * @param MapOptions Opçoes do Objeto mapa https://developers.google.com/maps/documentation/javascript/reference?hl=#MapOptions
-     */
-    NewGmap: function(div, MapOptions) {
-        if (!div) {
-            console.error('Nenhuma div encontrada');
-            return false;
-        }
-
-        var Gmap = {
-            /**
-             * Armazena o objeto Google Map
-             * google.maps.Map
-             * https://developers.google.com/maps/documentation/javascript/reference?hl=#Map
-             */
-            Map: null,
-            /**
-             *Array para armazenar objetos google.maps.InfoWindow
-             * https://developers.google.com/maps/documentation/javascript/reference#InfoWindow
-             */
-            InfoWindows: null,
-            /**
-             * Array para armazenar objetos google.maps.Marker
-             * https://developers.google.com/maps/documentation/javascript/reference#Marker
-             */
-            Markers: null,
-            /**
-             * Armazena um objeto google.maps.Polyline
-             * https://developers.google.com/maps/documentation/javascript/reference#Polyline
-             */
-            Polylines: null,
-            /**
-             * Distancia da rota
-             */
-            Distance: 0
-        };
-
-        var Options = GmapsTools.options.MapOptions;
-        if (typeof MapOptions === 'object')
-            Options = Tools.MergeRecursive(Options, MapOptions);
-
-        //Inicia o map
-        Gmap.Map = new google.maps.Map(div, Options);
-
-        return Gmap;
-    },
-    NewMarker: function(Gmap, MarkerOptions) {
-        var Options = GmapsTools.options.MarkerOptions;
-        if (typeof MarkerOptions === 'object')
-            Options = Tools.MergeRecursive(Options, MarkerOptions);
-
-        if (typeof Options.position[0] !== 'undefined' && Options.position[1] !== 'undefined')
-            Options.position = new google.maps.LatLng(Options.position[0], Options.position[1]);
-
-        Options.map = Gmap.Map;
-
-        if (Gmap.Markers === null)
-            Gmap.Markers = new Array();
-
-        Gmap.Markers.push(new google.maps.Marker(Options));
-    },
-    LoadMarkersByArray: function(Gmap, Markers, MarkerOptions) {
-        var MarkersLen = Markers.length;
-
-        for (var a = 0; a < MarkersLen; a++) {
-            MarkerOptions = typeof MarkerOptions === 'object' ? Tools.MergeRecursive(MarkerOptions, Markers[a]) : Markers[a];
-            GmapsTools.NewMarker(Gmap, MarkerOptions);
-        }
-    },
-    NewInfoWindow: function(Gmap, InfoWindowOptions) {
-        var Options = GmapsTools.options.InfoWindowOptions;
-
-        if (typeof InfoWindowOptions === 'object')
-            Options = Tools.MergeRecursive(Options, InfoWindowOptions);
-
-        if (typeof Options.position !== 'undefined' && typeof Options.position[0] !== 'undefined' && Options.position[1] !== 'undefined')
-            Options.position = new google.maps.LatLng(Options.position[0], Options.position[1]);
-
-        Options.map = Gmap.Map;
-
-        if (Gmap.InfoWindows === null)
-            Gmap.InfoWindows = new Array();
-
-        Gmap.InfoWindows.push(new google.maps.InfoWindow(Options));
-
-        var kInfo = Gmap.InfoWindows.length - 1;
-
-        if (Options.closed === true)
-            Gmap.InfoWindows[kInfo].close();
-    },
-    LoadInfoWindowsByArray: function(Gmap, InfoWindows, InfoWindowOptions) {
-        var InfoWindowsLen = InfoWindows.length;
-
-        for (var a = 0; a < InfoWindowsLen; a++) {
-            InfoWindowOptions = typeof InfoWindowOptions === 'object' ? Tools.MergeRecursive(InfoWindowOptions, InfoWindows[a]) : InfoWindows[a];
-            GmapsTools.NewInfoWindow(Gmap, InfoWindows[a]);
-        }
-    },
-    /**
-     * Cria um marcador com uma InfoWindow relacionada
-     * @param {GmapsTools.Gmaps} Gmap
-     * @param {object{position,content}} MarkerInfo
-     * @param {google.maps.MarkerOptions} MarkerOptions
-     * @param {google.maps.InfoWindowOptions} InfoWindowOptions
-     * @returns {void}
-     */
-    NewMarkerInfo: function(Gmap, MarkerInfo, MarkerOptions, InfoWindowOptions) {
-        if (typeof MarkerInfo !== 'object')
-            return false;
-
-        if (typeof MarkerInfo.position !== 'object' || typeof MarkerInfo.content !== 'string')
-            return false;
-
-        if (typeof MarkerOptions !== 'object')
-            MarkerOptions = {};
-
-        if (typeof InfoWindowOptions !== 'object')
-            InfoWindowOptions = {};
-
-        MarkerOptions.position = MarkerInfo.position;
-        GmapsTools.NewMarker(Gmap, MarkerOptions);
-        var kMarker = Gmap.Markers.length - 1;
-
-        InfoWindowOptions.content = MarkerInfo.content;
-        GmapsTools.NewInfoWindow(Gmap, InfoWindowOptions);
-        var kInfo = Gmap.InfoWindows.length - 1;
-
-        if (Gmap.InfoWindows[kInfo].closed !== true)
-            Gmap.InfoWindows[kInfo].open(Gmap.Map, Gmap.Markers[kMarker]);
-
-        GoogleLibs.Event.addListener(Gmap.Markers[kMarker], 'click', function() {
-            Gmap.InfoWindows[kInfo].open(Gmap.Map, Gmap.Markers[kMarker]);
-        });
-    },
-    /**
-     * Carrega varios MarkerInfos
-     * @param {GmapsTools.Gmaps} Gmap
-     * @param {object[]{position,content}} MarkerInfos
-     * @param {google.maps.MarkerOptions} MarkerOptions
-     * @param {google.maps.InfoWindowOptions} InfoWindowOptions
-     */
-    LoadMarkerInfos: function(Gmap, MarkerInfos, MarkerOptions, InfoWindowOptions) {
-        var MarkerInfosLen = MarkerInfos.length;
-
-        for (var a = 0; a < MarkerInfosLen; a++) {
-            GmapsTools.NewMarkerInfo(Gmap, MarkerInfos[a], MarkerOptions, InfoWindowOptions);
-        }
-    },
-    /**
-     * Carrega uma rota apartir de uma url codificada do google
-     * https://developers.google.com/maps/documentation/javascript/geometry?hl=pt-br
-     * @param Gmap object
-     * @param Path string|array
-     * @param PolylineOptions object
-     */
-    NewPolyline: function(Gmap, Path, PolylineOptions) {
-        if (!Path)
-            throw '@param Path não informado';
-        if (typeof Gmap !== 'object')
-            throw '@param Gmap não é um object';
-
-        if (typeof PolylineOptions !== 'object')
-            PolylineOptions = GmapsTools.options.PolylineOptions;
-
-        if (Gmap.Polylines === null)
-            Gmap.Polylines = new Array(new google.maps.Polyline());
-
-        //Pega a ultima Polyline que foi adicionada
-        var Polyline = Gmap.Polylines[Gmap.Polylines.length - 1];
-
-        //Seta as opções da Polyline de rota
-        Polyline.setOptions(PolylineOptions);
-
-        //Seta o mapa da Polyline de rota
-        Polyline.setMap(Gmap.Map);
-
-        switch (typeof Path) {
-            case 'string':
-                //Passa um MVCArray para o Polyline da rota
-                Polyline.setPath(GoogleLibs.Geometry.encoding.decodePath(Path));
-                break;
-            case 'object':
-                Polyline.setPath(GmapsTools.ArrayToMVCArray(Path));
-                break;
-        }
-
-        //Calcula a distancia da rota
-        Gmap.Distance = GoogleLibs.Geometry.spherical.computeLength(Polyline.getPath());
-
-        //Centraliza o mapa
-        GmapsTools.CenterMapByPath(Gmap);
-    },
-    /**
-     * Centraliza o mapa baseado na rota atual
-     * @param Gmap
-     */
-    CenterMapByPath: function(Gmap) {
-        //Pega o objeto path
-        var path = Gmap.Polylines[0].getPath();
-
-        //Instancia um objeto LatLngBounds
-        //https://developers.google.com/maps/documentation/javascript/reference?hl=pt-br#LatLngBounds
-        var bounds = new google.maps.LatLngBounds();
-
-        path.forEach(function(e) {
-            bounds.extend(e);
-        });
-
-        Gmap.Map.fitBounds(bounds);
-    },
-    /**
-     * Adiciona um StreetView ao mapa
-     * https://developers.google.com/maps/documentation/javascript/reference?hl=#StreetViewPanorama
-     * @param {object} Gmap
-     * @param {object} StreetViewOptions
-     * @returns object
-     */
-    AddStreetView: function(Gmap, StreetViewOptions) {
-        if (typeof StreetViewOptions === 'object')
-            StreetViewOptions = Tools.MergeRecursive(GmapsTools.options.StreetViewOptions, StreetViewOptions);
-
-        if (StreetViewOptions.position === null)
-            StreetViewOptions.position = Gmap.Map.getCenter();
-
-        Gmap.Map.setStreetView(new google.maps.StreetViewPanorama(Gmap.Map.getDiv(), StreetViewOptions));
-    },
-    /**
-     * Retorna um objeto MVCArray baseado em um Array 
-     * @param {array} array
-     * @returns {google.maps.MVCArray}
-     */
-    ArrayToMVCArray: function(array) {
-        var PathLen = array.length;
-
-        for (a = 0; a < PathLen; a++) {
-            array[a] = new google.maps.LatLng(Number(array[a][0]), Number(array[a][1]));
-        }
-
-        return new google.maps.MVCArray(array);
     }
 };
+
+function Gmap(div, options) {
+    console.trace();
+    /**
+     * Armazena o objeto Google Map
+     * https://developers.google.com/maps/documentation/javascript/reference?hl=#Map
+     * @type google.maps.Map 
+     */
+    var map;
+    /**
+     *Array para armazenar objetos google.maps.InfoWindow
+     * https://developers.google.com/maps/documentation/javascript/reference#InfoWindow
+     * @type Array
+     */
+    var InfoWindows;
+    /**
+     * Array para armazenar objetos google.maps.Marker
+     * https://developers.google.com/maps/documentation/javascript/reference#Marker
+     * @type Array
+     */
+    var Markers;
+    /**
+     * Armazena um objeto google.maps.Polyline
+     * https://developers.google.com/maps/documentation/javascript/reference#Polyline
+     * @type Array
+     */
+    var Polylines;
+
+    //@construct
+    this.InfoWindows = null;
+    this.Markers = null;
+    this.Polylines = null;
+
+    //Verifica se @param div é um objeto html
+    if (typeof div !== 'object' && div.nodeType !== 1)
+        return false;
+
+    //Define as opções dos objetos do GoogleMaps
+    options = {
+        MapOptions: typeof options === 'object' && typeof options.MapOptions === 'object' ?
+                Tools.MergeRecursive(GmapsTools.options.MapOptions, options.MapOptions) :
+                GmapsTools.options.MapOptions
+    };
+
+    //Cria o mapa
+    this.map = new google.maps.Map(div, options.MapOptions);
+    //@end
+
+    //@implementation
+    /**
+     * Centraliza o mapa baseado em latitude e longitude ou endereço textual
+     * @param {string|object(google.maps.LatLng)} center
+     * @returns {Boolean}
+     */
+    this.setCenter = function(center) {
+        //Verifica o tipo do @param center passado
+        switch (typeof center) {
+            case 'object':
+                if (typeof center.lat === 'object' && typeof center.lng === 'object')
+                    this.map.setCenter(center);
+                else
+                    return false;
+                break;
+            case 'string':
+                //Código para centralizar pelo endereço
+                return false;
+                break;
+            default:
+                return false;
+                break;
+        }
+    };
+
+    /**
+     * Retorna a latitude e longitude do centro do mapa
+     * Alias para google.maps.Map.getCenter()
+     * @returns {google.maps.LatLng}
+     */
+    this.getCenter = function() {
+        return this.map.getCenter();
+    };
+
+    //@end
+}
